6d119c9dedf07ef25c2bfa4755454fb0c94b01b5
dengyongqiang <dengyongqiang1989@163.com>
2017-02-08 23:38:52 +0800

    This is the first Demo

---
+++ src/demo/HelloDate.java
@@ -0,0 +1,10 @@
+package demo;
+
+import java.util.Date;
+
+public class HelloDate {
+	public static void main(String[] args) {
+		System.out.println("Hello, it's");
+		System.out.println(new Date());
+	}
+}
