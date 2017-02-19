2aec40fbed7d8d453e474dd2efe50eb162cfe1cc
dengyongqiang <dengyongqiang1989@163.com>
2017-02-19 20:28:57 +0800

    Static import demo

---
+++ src/operators/StaticImportDemo.java
@@ -0,0 +1,10 @@
+package operators;
+import static java.lang.System.out;
+public class StaticImportDemo {
+
+	public static void main(String[] args) {
+		out.println("Hello Master");
+		System.out.println("Hello Master");
+	}
+
+}
