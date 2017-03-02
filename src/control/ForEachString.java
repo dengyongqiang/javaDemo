e13b23b5f41004de702f81a2e09deda77c8a5f2b
dengyongqiang <dengyongqiang1989@163.com>
2017-03-03 00:20:13 +0800

    ForEachDemo external jar net.mindview.util

---
+++ src/control/ForEachString.java
@@ -0,0 +1,11 @@
+package control;
+
+public class ForEachString {
+	
+	public static void main(String[] args) {
+		for (char c : "An African Swallow".toCharArray()) {
+			System.out.print(c + " ");
+		}
+	}
+
+}
