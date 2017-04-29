bc04a7045ca4840c17d670fc67c0b87026614d95
dengyongqiang <dengyongqiang1989@163.com>
2017-04-29 12:56:17 +0800

    Creating an array of nonprimitive objects

---
+++ src/initialization/ArrayClassObj.java
@@ -0,0 +1,21 @@
+package initialization;
+
+import static net.mindview.util.Print.print;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class ArrayClassObj {
+
+	public static void main(String[] args) {
+		Random rand = new Random(47);
+		Integer[] a = new Integer[rand.nextInt(20)];
+		print("length of a = " + a.length);
+		for (int i = 0; i < a.length; i++) {
+			a[i] = rand.nextInt(500);
+		}
+		print(Arrays.toString(a));
+
+	}
+
+}
