d10bfa38ca6ee9da6fa0e10d8cf341f42048eeb8
dengyongqiang <dengyongqiang1989@163.com>
2017-04-29 13:03:10 +0800

    ArrayInitialization

--- src/initialization/ArrayInit.java
+++ src/initialization/ArrayInit.java
@@ -6,7 +6,7 @@ public class ArrayInit {
 
 	public static void main(String[] args) {
 		Integer[] a = { new Integer(1), new Integer(2), 3, };
-		Integer[] b = { new Integer(1), new Integer(2), 3, };
+		Integer[] b = new Integer[] { new Integer(1), new Integer(2), 3, };
 		System.out.println(Arrays.toString(a));
 		System.out.println(Arrays.toString(b));
 	}
