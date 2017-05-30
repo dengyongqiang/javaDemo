c4b9ec88b81f403a3c0b26feb6f85cfbe0397915
dengyongqiang <dengyongqiang1989@163.com>
2017-05-30 16:11:32 +0800

    Constructor calls during inheritance.

---
+++ src/reusing/Cartoon.java
@@ -0,0 +1,28 @@
+package reusing;
+
+import static net.mindview.util.Print.print;
+
+class Art {
+	Art() {
+		print("Art constructor");
+	}
+
+}
+
+class Drawing extends Art {
+	Drawing() {
+		print("Drawing constructor");
+	}
+}
+
+public class Cartoon extends Drawing {
+
+	public Cartoon() {
+		print("Cartoon constructor");
+	}
+
+	public static void main(String[] args) {
+		Cartoon x = new Cartoon();
+	}
+
+}
